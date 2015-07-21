45cb3c1b24f36bdaeea8a3df6c3a4a98d29f27a9
DylanAndrews & Andrew Carr <pair+DylanAndrews.aacarr5@devbootcamp.com>
2015-07-21 13:17:38 -0500

    Partial

--- recursion.rb
+++ recursion.rb
@@ -1,4 +1,4 @@
-# require 'pry'
+require 'pry'
 
 class Game
 
@@ -11,50 +11,56 @@ class Game
 
 	def create_board(board)
 		board[position[:x]] = "X"
-		board
 	end
 
-	def play_game
-		validate_move(gets_move)
-		create_board(board)
+	def update_board(board,input)
+		board[positioon[:x]] = 'X'
+		if input == "A"
+			board[position[:x]+1] = " "
+		elsif input == "D"
+			board[position[:x]-1] = " "
+		end
+	end
+
+	def play_game(full_board=@board)
+		move = validate_move(gets_move)
+		update_board(board,move)
 		p print_board
+		play_game(board)
 	end
 
 	def gets_move
-		print "Press 'D' to move your piece!"
-		move = gets.chomp
+		print "Press A/D/W/X to move your piece! "
+		gets.chomp.upcase!
 	end
 
 	def validate_move(move)
-		move = move.upcase
-		if move != "A" || move != "D" || move != "W" || move != "Z" 
-			puts "Sorry, please choose a valid position" 
-			gets_move
+		if move != "A" && move != "D" && move != "W" && move != "Z"
+			puts "Sorry, please choose a valid position"
+			binding.pry
+			validate_move(gets_move)
 		end
-
 		if move == "A"
-			gets_move if position[:x] - 1 < 0
+			validate_move(gets_move) if position[:x] - 1 < 0
 		elsif move == "D"
-			gets_move if position[:x] + 1 > board.first.length
+			validate_move(gets_move) if position[:x] + 1 > board.length
 		end
 		move_to_position(move)
+		move
 	end
 
 	def move_to_position(input)
-		# binding.pry
 		if input == "D"
-			gets_move if position[:x] >= board.first.length - 1
 			position[:x]+=1
+		elsif input == "A"
+			position[:x]-=1
 		end
+		# input
 	end
 
 	def print_board
 		board.each {|line| line}
 	end
-
-	# def update_board
-	# end
-
 
 
 end
--- recursion_spec.rb
+++ recursion_spec.rb
@@ -1,11 +1,14 @@
 require_relative 'recursion'
 
-describe 'The Recursion Game' do 
+describe 'The Recursion Game' do
 	let(:game) {Game.new([" "," "," ","$"])}
 
-	describe '#initialize' do 
-		it 'should expect to create a board' do 
+	describe '#initialize' do
+		it 'should expect to create a board' do
 			expect(game.board).to eq(["X"," "," ","$"])
 		end
 	end
+
+  # describe '#validate_move'
+
 end
